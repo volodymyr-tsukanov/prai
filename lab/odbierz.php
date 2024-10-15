8bad84e9d82dc9f92b3eb6a011ce1977be78d1b8
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-10-15 21:04:23 +0200

    zad2.1

---
+++ lab/odbierz.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Form receiver</title>
+</head>
+
+<body>
+    <div>
+        <h2>Dane odebrane z formularza:</h2>
+        <?php
+        if (isset($_REQUEST['nazw']) && ($_REQUEST['nazw'] != "")) {
+            $nazwisko = htmlspecialchars(trim($_REQUEST['nazw']));
+            echo "Nazwisko: $nazwisko <br />";
+        } else echo "Nie wpisano nazwiska <br />";
+
+        if (isset($_REQUEST['wiek']) && ($_REQUEST['wiek'] != "")) {
+            $wiek = htmlspecialchars(trim($_REQUEST['wiek']));
+            echo "Wiek: $wiek <br />";
+        } else echo "Nie wpisano wiek <br />";
+
+        if (isset($_REQUEST['kraj']) && ($_REQUEST['kraj'] != "")) {
+            $kraj = htmlspecialchars(trim($_REQUEST['kraj']));
+            echo "Kraj: $kraj <br />";
+        } else echo "Nie wpisano kraj <br />";
+
+        if (isset($_REQUEST['email']) && ($_REQUEST['email'] != "")) {
+            $email = htmlspecialchars(trim($_REQUEST['email']));
+            echo "Email: $email <br />";
+        } else echo "Nie wpisano emailiska <br />";
+
+        $tut = '';
+        if (isset($_REQUEST['php']) && ($_REQUEST['php'] == "on"))
+            $tut = 'php';
+        if (isset($_REQUEST['c']) && ($_REQUEST['c'] == "on"))
+            $tut = $tut.' c/c++';
+        if (isset($_REQUEST['java']) && ($_REQUEST['java'] == "on"))
+            $tut = $tut.' java';
+        echo "Tutorial: $tut <br />";
+
+        if (isset($_REQUEST['zaplata']) && ($_REQUEST['zaplata'] != "")) {
+            $zaplata = htmlspecialchars(trim($_REQUEST['zaplata']));
+            echo "Sposób zapaty: $zaplata <br />";
+        } else echo "Nie używaj konsoli! <br />";
+        ?>
+    </div>
+</body>
+
+</html>
---
+++ lab/odbierz2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Form receiver</title>
+</head>
+
+<body>
+    <div>
+        <h2>Dane odebrane z formularza:</h2>
+        <?php
+        foreach ($_REQUEST as $key => $value) {
+            echo "$key = $value <br />";
+        }
+
+        echo "<h3>GET:</h3>";
+        var_dump($_GET);
+        echo "<h3>POST:</h3>";
+        var_dump($_POST);
+        echo "<h3>REQUEST:</h3>";
+        var_dump($_REQUEST);
+        ?>
+    </div>
+</body>
+
+</html>
