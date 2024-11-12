348ede6a72c0f76451cb72c76cfde30314a43e7a
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-11-12 18:45:36 +0100

    fixes lab3p2

--- lab/zdjecia.php
+++ lab/zdjecia.php
@@ -1,17 +1,24 @@
 <?php
-if (isset($_POST['zapisz']) && $_POST['zapisz'] == 'Zapisz' && isset($_GET['pic'])){
+include_once "funkcje.php";
+
+
+$imgPath = './data/img/';
+$thumbPath = './data/tnls/';  //thumbnails
+
+
+setDebugMode(1);
+printHTMLhead('Img thumbs');
+
+if (isset($_POST['zapisz']) && $_POST['zapisz'] == 'Zapisz' && !isset($_GET['pic'])){
     if (is_uploaded_file($_FILES['zdjecie']['tmp_name'])) {
         $typ = $_FILES['zdjecie']['type'];
         if ($typ === 'image/jpeg') {
-            move_uploaded_file($_FILES['zdjecie']['tmp_name'], './' .
-            $_FILES['zdjecie']['name']);
-            
-            $link = $_FILES['zdjecie']['name'];
-            $random = uniqid('img_'); //wygenerowanie losowej wartości
-            $zdj = $random . '.jpg';
-            copy($link, './' . $zdj); //utworzenie kopii zdjęcia
+            $imgId = uniqid(); //wygenerowanie losowej wartości
+            $iPath = $imgPath . 'img_' . $imgId . '.jpg';
+            $tPath = $thumbPath . 'thn_' . $imgId . '.jpg';
+            move_uploaded_file($_FILES['zdjecie']['tmp_name'], $iPath);
 
-            list($width, $height) = getimagesize($zdj); //pobranie rozmiarów obrazu
+            list($width, $height) = getimagesize($iPath); //pobranie rozmiarów obrazu
             $wys = $_POST['wys']; //wysokość preferowana przez użytkownika
             $szer = $_POST['szer']; //szerokość preferowana przez użytkownika
             $skalaWys = 1;
@@ -27,31 +34,30 @@ if (isset($_POST['zapisz']) && $_POST['zapisz'] == 'Zapisz' && isset($_GET['pic'
 
             header('Content-Type: image/jpeg');
             $nowe = imagecreatetruecolor($newW, $newH); //czarny obraz
-            $obraz = imagecreatefromjpeg($zdj);
-            imagecopyresampled($nowe, $obraz, 0, 0, 0, 0,
-            $newW, $newH, $width, $height);
-            imagejpeg($nowe, './mini-' . $link, 100);
-            echo "nowe=/mini-$link <br>";
+            $obraz = imagecreatefromjpeg($iPath);
+            imagecopyresampled($nowe, $obraz, 0, 0, 0, 0, $newW, $newH, $width, $height);
+            imagejpeg($nowe, $tPath, 100);
+            echo "Miniatura utworzona: <a href='$tPath'>Zobacz miniaturę</a><br>";
             imagedestroy($nowe);
             imagedestroy($obraz);
-            unlink($zdj);
 
-            $dlugosc = strlen($link);
-            $dlugosc -= 4;
-            $link = substr($link, 0, $dlugosc);
-            echo "link=$link <br/>";
-            header('location:zdjecia.php?pic='.$link);
+            header('location:zdjecia.php?pic='.$imgId);
+            exit;
         } else {
             header('location:zdjecia.html');
+            exit;
         }
     }
 }
 
 if (isset($_GET['pic']) && !empty($_GET['pic'])){
-    echo '<a href="' . $_GET['pic'] . '.jpg">Zdjęcie</a><br/>';
-    echo '<a href="mini-' . $_GET['pic'] . '.jpg">Miniatura</a><br/><br/>';
+    echo '<a href="' . $imgPath.'img_'. htmlspecialchars($_GET['pic']) . '.jpg">Zdjęcie</a><br>';
+    echo '<a href="' . $thumbPath.'thn_' . htmlspecialchars($_GET['pic']) . '.jpg">Miniatura</a><br><br>';
     echo '<a href="zdjecia.html">Powrót</a>';
 }
+echo"<br>";
+
+printHTMLtail();
 
 // /etc/php/8.3/cli/php.ini <(line 943)- extension=gd
 ?>
