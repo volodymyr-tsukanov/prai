c9d873404d16a868e49df2fa228dc7f300ef0e9e
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-11-22 13:50:32 +0100

    zad6.1

--- lab/index.php
+++ lab/index.php
@@ -1,36 +1,51 @@
 <?php
 namespace prai_lab;
 
-include_once "funkcje.php";
-include_once "php_classes/User.php";
-include_once "php_classes/RegistrationForm.php";
+include_once 'funkcje.php';
+require_once 'php_classes/DTBase.php';
 
-$usersPath = 'data/users.';
+
+$jezyki = ["C", "CPP", "Java", "C#", "HTML", "CSS", "XML", "PHP", "JavaScript"];
+$zaplaty = ["eurocard", "visa", "przelew"];
+$akcje = ["Wyczyść", "Zapisz", "Pokaż", "PHP", "CPP", "Java", "Staty"];
+
+$submit = isset($_POST['submit']) ? $_POST['submit'] : null;
+
+
+function printForm(){
+    global $jezyki, $zaplaty, $akcje;
+    echo '<form action="pliki.php" method="POST">';
+    echo '<table>';
+    echo '<tr><td>Nazwisko: </td><td><input name="nazw" size="30" id="nazw"/></td></tr>';
+    echo '<tr><td>Wiek:</td><td><input name="wiek" size="30" id="wiek"/></td></tr>';
+    echo '<tr><td>Państwo:</td><td>';
+    echo '<select name="kraj" id="kraj">';
+    echo '<option value="pl" selected="selected">Polska</option>';
+    echo '<option value="gb">Wielka Brytania</option>';
+    echo '</select></td></tr>';
+    echo '<tr><td>Adres e-mail: </td><td><input name="email" size="30" id="email"/></td></tr>';
+    echo '</table>';
+    echo '<h4>Zamawiam tutorial z języka:</h4>';
+    foreach ($jezyki as $jezyk) {
+        echo '<input name="jezyki[]" type="checkbox" value="'.$jezyk.'" />'.$jezyk.' ';
+    }
+    echo '<h4>Sposób zapłaty:</h4>';
+    foreach ($zaplaty as $zaplata){
+        echo "<input name='zaplata' type='radio' value='$zaplata' />$zaplata";
+    }
+    echo '<input name="zaplata" type="radio" value="cash" checked />gotówką 🤫';
+    echo '<br>';
+    foreach ($akcje as $akcja){
+        echo "<input type='submit' name='submit' value='$akcja' />";
+    }
+    echo '</form>';
+}
 
 
 setDebugMode(1);
-printHTMLhead('Lab5', true);
+printHTMLhead('Lab5', false);
 
-$user1 = new User ('kp', 'Kubus Puchatek', 'kubus@stumilowylas.pl', 'nielubietygryska');
-$user1->show();
-//$user1->save($usersPath.User::FORMAT_XML,User::FORMAT_XML);
-
-$rf = new RegistrationForm(); //wyświetla formularz rejestracji
-if (filter_input(INPUT_POST, 'submit', FILTER_SANITIZE_FULL_SPECIAL_CHARS)) {
-    $user = $rf->checkUser(); //sprawdza poprawność danych
-    if ($user === NULL)
-        echo "<p>Niepoprawne dane rejestracji.</p>";
-    else{
-        echo "<p>Poprawne dane rejestracji:</p>";
-        $user->show();
-        $user->save($usersPath.User::FORMAT_JSON);
-    }
-}
-
-print('<br>JSON:<br>');
-$user1->getAllUsers($usersPath.User::FORMAT_JSON);
-print('<br><br>XML:<br>');
-$user1->getAllUsers($usersPath.User::FORMAT_XML,User::FORMAT_XML);
+printForm();
 
 printHTMLtail();
 ?>
---
+++ lab/php_classes/DTBase.php
@@ -0,0 +1,12 @@
+<?php
+namespace prai_lab;
+
+
+class DTBase
+{
+
+}
+
+//    SQL
+// INSERT INTO `klienci`(`Id`, `Nazwisko`, `Wiek`, `Panstwo`, `Email`, `Zamowienie`, `Platnosc`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]')
+?>
